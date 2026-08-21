365ad0dd7c6d404d3e101a4e534d95c16be0e18b
RENANTE T. MATEO JR. <renantemateo18@gmail.com>
2026-08-21 16:43:41 +0800

    add greatest number code

---
+++ WhoIsTheGreatestNumber/src/whoisthegreatestnumber/WhoIsTheGreatestNumber.java
@@ -0,0 +1,24 @@
+package whoisthegreatestnumber;
+public class WhoIsTheGreatestNumber {
+
+    public static void main(String[] args) {
+        
+                int num1 = 25;
+        int num2 = 87;
+        int num3 = 78;
+
+        if (num1 > num2 && num1 > num3) {
+            System.out.println("The Greatest Number is : " + num1);
+        }
+        else if (num2 > num1 && num2 > num3) {
+            System.out.println("The Greatest Number is : " + num2);
+        }
+        else {
+            System.out.println("The Greatest Number is : " + num3);
+
+}
+        
+        // TODO code application logic here
+    }
+    
+}
